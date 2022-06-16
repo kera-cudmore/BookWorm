3b94f90b6d98e43e97afea8c922b4dcb53ec82eb
Kera Cudmore <kera.cudmore@hotmail.com>
2022-06-16 18:35:00 +0000

    add: imports to the __init__.py, set up an instance of flask class & setup app config variables, added database variable

---
+++ bookworm/__init__.py
@@ -0,0 +1,22 @@
+"""
+IMPORT os, Flask & SQLAlchemy
+"""
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+# IMPORT env if there is an env.py file
+# Used in local dev as not pushed to github & heroku
+if os.path.exists("env.py"):
+    import env # noqa
+
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+db = SQLAlchemy(app)
+
+from bookworm import routes # noqa
