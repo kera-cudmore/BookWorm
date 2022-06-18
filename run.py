ff29bca9526026eae73a1cf531663eea10f1c6cd
Kera Cudmore <kera.cudmore@hotmail.com>
2022-06-18 14:43:58 +0000

    fix: added missing underscores to main

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ import os
 from bookworm import app
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
