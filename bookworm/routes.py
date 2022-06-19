8cfd33c02470f3d1dec80b7b205b93f5646da0d3
Kera Cudmore <kera.cudmore@hotmail.com>
2022-06-19 11:47:19 +0000

    add: login and register routes to return render_template to allow styling of page

--- bookworm/routes.py
+++ bookworm/routes.py
@@ -14,3 +14,13 @@ def home():
     Home function will return the rendered template for base.html
     """
     return render_template("base.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
