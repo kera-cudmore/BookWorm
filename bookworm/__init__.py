8ea1428894c39b2c78d9ce5802a48bbeb483bc0c
Kera Cudmore <kera.cudmore@hotmail.com>
2022-06-20 19:04:00 +0000

    fix: added extra line & comment to route import

--- bookworm/__init__.py
+++ bookworm/__init__.py
@@ -14,9 +14,10 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 db = SQLAlchemy(app)
 
-from bookworm import routes # noqa
+from bookworm import routes # noqa - app & db need to be defined first
