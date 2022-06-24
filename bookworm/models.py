4a38adf6d75a00e2688a12abbbc8a3c9c8824879
Kera Cudmore <kera.cudmore@hotmail.com>
2022-06-24 01:51:07 +0000

    tidied up code in models

--- bookworm/models.py
+++ bookworm/models.py
@@ -1,34 +1,28 @@
-"""
-IMPORTS
-db from bookworm - to define the database structure
-"""
+""" IMPORTS """
 
+# To define the database structure
 from bookworm import db
 
 
 class Users(db.Model):
-    # Schema for the Users model
+    """ Schema for the Users model """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(260), unique=True, nullable=False) 
     # password string is 260 chars so hashed password can be stored in db
+    password = db.Column(db.String(260), unique=True, nullable=False)
 
     def __repr__(self):
-            # __repr__ to represent itself in the form of a string
-            return self.username
-
-
-
+        """ to represent itself in the form of a string """
+        return self.users
 
 
 class Bookshelves(db.Model):
-    # Schema for the Bookshelves model
+    """ Schema for the Bookshelves model """
     id = db.Column(db.Integer, primary_key=True)
     shelf_name = db.Column(db.String(25), unique=True, nullable=False)
-    
-    
+
     def __repr__(self):
-        # __repr__ to represent itself in the form of a string
+        """ to represent itself in the form of a string """
         return self.shelf_name
