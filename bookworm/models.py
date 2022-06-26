4e2180b586f4746f672981dbf9425fe092c0a8bd
Kera Cudmore <kera.cudmore@hotmail.com>
2022-06-26 14:26:19 +0000

    removed name column from db

--- bookworm/models.py
+++ bookworm/models.py
@@ -8,7 +8,6 @@ class Users(db.Model):
     """ Schema for the Users model """
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     # password string is 260 chars so hashed password can be stored in db
     password = db.Column(db.String(260), unique=True, nullable=False)
