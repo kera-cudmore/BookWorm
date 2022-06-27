6c7abbb1b11388adc3cb61c3dde23750cf9d1fe7
Kera Cudmore <kera.cudmore@hotmail.com>
2022-06-27 23:03:59 +0000

    added route for searching google books api WIP

--- bookworm/books/routes.py
+++ bookworm/books/routes.py
@@ -1,14 +1,29 @@
 """ IMPORTS """
 from flask import render_template, request, Blueprint, url_for, flash, redirect, session
-from bookworm import db
+import requests
+import json
+from bookworm import os, db
 from bookworm.models import Users, Bookshelves
+
+api_key = os.environ.get("GOOGLE_BOOKS_API") 
 
 books = Blueprint('books', __name__)
 
 
-@books.route("/search")
+@books.route("/search", methods=["GET", "POST"])
 def search():
     """
     SEARCH FUNCTION
     """
+    if request.method == "POST":
+        payload = {}
+        payload["q"] = request.form.get("searchquery")
+        payload["key"] = os.environ.get("GOOGLE_BOOKS_API")
+
+        book_request = requests.get("https://www.googleapis.com/books/v1/volumes", params=payload)
+
+        results = book_request.json()
+
+        print(results)
+
     return render_template("search.html")
