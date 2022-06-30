4190adf29a4143fa4b905ded4b1a5a6865c0a96c
Kera Cudmore <kera.cudmore@hotmail.com>
2022-06-30 14:42:24 +0000

    added fields to the api request url to only return fields required

--- bookworm/books/routes.py
+++ bookworm/books/routes.py
@@ -1,7 +1,7 @@
 """ IMPORTS """
+# import json
 from flask import render_template, request, Blueprint, url_for, flash, redirect, session
 import requests
-import json
 from bookworm import os, db
 from bookworm.models import Users, Bookshelves
 
@@ -20,17 +20,19 @@ def search():
         payload["q"] = request.form.get("searchquery")
         payload["key"] = os.environ.get("GOOGLE_BOOKS_API")
 
-        book_request = requests.get("https://www.googleapis.com/books/v1/volumes", params=payload)
+        book_request = requests.get("https://www.googleapis.com/books/v1/volumes?fields=items.volumeInfo(title,authors,description,imageLinks/thumbnail)", params=payload)
         results = book_request.json()
-
         print(results)
+    
         return render_template("search.html", results=results)
-       
+
     return render_template("search.html")
 
 
 @books.route("/bookshelves")
 def bookshelves():
-
+    """
+    BOOKSHELVES FUNCTION
+    """
 
     return render_template("bookshelves.html")
