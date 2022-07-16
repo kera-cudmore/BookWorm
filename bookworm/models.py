ffaf358f88053d0d4fc8fc0578ac7b0f0100f26e
Kera Cudmore <kera.cudmore@hotmail.com>
2022-07-16 16:27:49 +0000

    added the created_by column to the bookshelves database

--- bookworm/models.py
+++ bookworm/models.py
@@ -20,7 +20,8 @@ class Users(db.Model):
 class Bookshelves(db.Model):
     """ Schema for the Bookshelves model """
     id = db.Column(db.Integer, primary_key=True)
-    shelf_name = db.Column(db.String(25), unique=True, nullable=False)
+    shelf_name = db.Column(db.String(50), unique=True, nullable=False)
+    created_by = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
         """ to represent itself in the form of a string """
