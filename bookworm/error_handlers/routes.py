7e5eef216607dedd7479588c78fb0204194cbf5a
Kera Cudmore <kera.cudmore@hotmail.com>
2022-07-28 18:29:48 +0000

    refactor: added docstrings to functions

--- bookworm/error_handlers/routes.py
+++ bookworm/error_handlers/routes.py
@@ -8,6 +8,7 @@ error = Blueprint('error', __name__)
 def handle_400(e):
     """
     400 ERROR FUNCTION
+    Takes in 400 error and returns the error page with error message for 400 error
     """
     error_message = "It looks like this page was accessed incorrectly, or may have be corrupted."
     return render_template('error.html', error_number="400", error_message=error_message)
@@ -17,6 +18,7 @@ def handle_400(e):
 def handle_401(e):
     """
     401 ERROR FUNCTION
+    Takes in 401 error and returns the error page with error message for 401 error
     """
     error_message = "It looks like you may not have authorisation to view this page."
     return render_template('error.html', error_number="401", error_message=error_message)
@@ -26,6 +28,7 @@ def handle_401(e):
 def handle_404(e):
     """
     404 ERROR FUNCTION - if address isn't right
+    Takes in 404 error and returns the error page with error message for 404 error
     """
     error_message = "The page you're looking for couldn't be found. Please check the page URL has been entered correctly"
     return render_template('error.html', error_number="404", error_message=error_message)
@@ -35,6 +38,7 @@ def handle_404(e):
 def handle_500(e):
     """
     500 ERROR FUNCTION
+    Takes in 500 error and returns the error page with error message for 500 error
     """
     error_message = "There was a problem while fulfilling your request."
     return render_template('error.html', error_number="500", error_message=error_message)
